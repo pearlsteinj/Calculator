941ab2d2d856a346215d6bca347006beda83909d
Josh <pearlsteinj@me.com>
2013-02-02 14:43:27 -0500

    added Design and interface

--- Calculator/JLPViewController.h
+++ Calculator/JLPViewController.h
@@ -9,5 +9,28 @@
 #import <UIKit/UIKit.h>
 
 @interface JLPViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *calcOutput;
+- (IBAction)buttonNum0:(UIButton *)sender;
+- (IBAction)buttonNum1:(UIButton *)sender;
+- (IBAction)buttonNum2:(UIButton *)sender;
+- (IBAction)buttonNum3:(UIButton *)sender;
+- (IBAction)buttonNum4:(UIButton *)sender;
+- (IBAction)buttonNum5:(UIButton *)sender;
+- (IBAction)buttonNum6:(UIButton *)sender;
+- (IBAction)buttonNum7:(UIButton *)sender;
+- (IBAction)buttonNum8:(UIButton *)sender;
+- (IBAction)buttonNum9:(UIButton *)sender;
+- (IBAction)buttonPeriod:(UIButton *)sender;
+- (IBAction)buttonEquals:(UIButton *)sender;
+- (IBAction)buttonPlus:(UIButton *)sender;
+- (IBAction)buttonMinus:(UIButton *)sender;
+- (IBAction)buttonMult:(UIButton *)sender;
+- (IBAction)buttonDivide:(UIButton *)sender;
+- (IBAction)buttonNeg:(UIButton *)sender;
+- (IBAction)buttonClear:(UIButton *)sender;
+- (IBAction)buttonPi:(UIButton *)sender;
+- (IBAction)buttonExponent:(UIButton *)sender;
+- (IBAction)buttonLParenthesis:(UIButton *)sender;
+- (IBAction)buttonRParenthesis:(UIButton *)sender;
 
 @end
--- Calculator/JLPViewController.m
+++ Calculator/JLPViewController.m
@@ -13,17 +13,87 @@
 @end
 
 @implementation JLPViewController
-
+@synthesize calcOutput;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //Add background Image
+    UIImage *patternImage = [UIImage imageNamed:@"low_contrast_linen.png"];
+    self.view.backgroundColor = [UIColor colorWithPatternImage:patternImage];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonNum7:(UIButton *)sender {
+}
+
+- (IBAction)buttonNum8:(UIButton *)sender {
+}
+
+- (IBAction)buttonNum9:(UIButton *)sender {
+}
+
+- (IBAction)buttonPeriod:(UIButton *)sender {
+}
+
+- (IBAction)buttonEquals:(UIButton *)sender {
+}
+
+- (IBAction)buttonPlus:(UIButton *)sender {
+}
+
+- (IBAction)buttonMinus:(UIButton *)sender {
+}
+
+- (IBAction)buttonMult:(UIButton *)sender {
+}
+
+- (IBAction)buttonDivide:(UIButton *)sender {
+}
+
+- (IBAction)buttonNeg:(UIButton *)sender {
+}
+
+- (IBAction)buttonClear:(UIButton *)sender {
+}
+
+- (IBAction)buttonPi:(UIButton *)sender {
+}
+
+- (IBAction)buttonExponent:(UIButton *)sender {
+}
+
+- (IBAction)buttonLParenthesis:(UIButton *)sender {
+}
+
+- (IBAction)buttonRParenthesis:(UIButton *)sender {
+}
+- (IBAction)buttonNum0:(UIButton *)sender {
+}
+
+- (IBAction)buttonNum1:(UIButton *)sender {
+}
+
+- (IBAction)buttonNum2:(UIButton *)sender {
+}
+
+- (IBAction)buttonNum3:(UIButton *)sender {
+}
+
+- (IBAction)buttonNum4:(UIButton *)sender {
+}
+
+- (IBAction)buttonNum5:(UIButton *)sender {
+}
+
+- (IBAction)buttonNum6:(UIButton *)sender {
 }
 
 @end
